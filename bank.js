9d042f63b6c7f5917939c2cb064b1f3bfa81dc28
Mahfuz <you@example.com>
2021-01-26 14:39:27 +0600

    bank data update

--- bank.js
+++ bank.js
@@ -10,28 +10,65 @@ loginButton.addEventListener("click", function () {
 
 //Deposit Button Event Handler
 
-const depositBtn=document.getElementById("add-deposit");
-depositBtn.addEventListener("click",function(){
-    const depositAmount = document.getElementById("depositAmount").value;
-    const depositNumber=parseFloat(depositAmount);
+    const depositBtn=document.getElementById("add-deposit");
+    depositBtn.addEventListener("click",function(){
+    // const depositAmount = document.getElementById("depositAmount").value;
+    // const depositNumber=parseFloat(depositAmount);
+    const depositNumber=getInputNumber("depositAmount");
 
-    const currentDeposit=document.getElementById("currentDeposit").innerText;
+
+        /*const currentDeposit=document.getElementById("currentDeposit").innerText;
         const currentDepositNumber=parseFloat(currentDeposit);
-
         const totalDeposit=depositNumber+currentDepositNumber;
-        // console.log(totalDeposit);
         document.getElementById("currentDeposit").innerText=totalDeposit;
+        */
 
-        const currentBalance=document.getElementById("currentBalance").innerText;
-        const currentBalanceNumber=parseFloat(currentBalance);
-        const totalBalance=depositNumber+currentBalanceNumber;
-        document.getElementById("currentBalance").innerText=totalBalance;
-        // console.log(totalBalance);
+        /* const currentBalance=document.getElementById("currentBalance").innerText;
+          const currentBalanceNumber=parseFloat(currentBalance);
+         const totalBalance=depositNumber+currentBalanceNumber;
+         document.getElementById("currentBalance").innerText=totalBalance;
+         console.log(totalBalance);*/
 
 
-
+        updateSpanText("currentDeposit",depositNumber);
+        updateSpanText("currentBalance",depositNumber);
         document.getElementById("depositAmount").value="";
-
-    
+       
 
 })
+
+function updateSpanText(id,depositNumber){
+
+  const current=document.getElementById(id).innerText;
+        const currentNumber=parseFloat(current);
+        const totalAmount=depositNumber+currentNumber;
+        document.getElementById(id).innerText=totalAmount;
+}
+
+
+//WithDraw Button Event Handler
+
+const withDrawBtn=document.getElementById("addWithDraw");
+withDrawBtn.addEventListener("click",function(){
+  // const withDrawAmount=document.getElementById("withDrawAmount").value;
+  // const withDrawNumber=parseFloat(withDrawAmount);
+  const withDrawNumber=getInputNumber("withDrawAmount");
+  updateSpanText("currentWithdraw",withDrawNumber);
+  updateSpanText("currentBalance",-1*withDrawNumber);
+  document.getElementById("withDrawAmount").value="";
+  
+  
+  
+  
+  
+  
+
+})
+
+function getInputNumber(id){
+  const amount=document.getElementById(id).value;
+  const amountNumber=parseFloat(amount);
+  return amountNumber;
+  
+
+}
